09205d96c02be3198fb8cdfcd97d0ca5a39514d5
MiguelTorresPerez <47027734+MiguelTorresPerez@users.noreply.github.com>
2022-05-20 01:15:38 +0200

    Update xnor.py

--- xnor.py
+++ xnor.py
@@ -1,9 +1,8 @@
 import math
 import numpy as np
 
-#logic gate xnor cnn approach
 
-##funct
+##sigmoid and forward Propagation functions
 sigmoid = lambda n : 1 / (1 + math.pow(math.e,-n))
 forwardP = lambda x, theta : [sigmoid(np.dot([1]+x,t)) for t in theta]
 
@@ -16,6 +15,7 @@ inputF = [float(x1),float(x2)]
 thetaMatrixOutputLayer = [[-10,20,20]]
 thetaMatrixHiddenLayer = [[-30,20,20],[10,-20,-20]]
 
+#theta vectors of each layer
 a2 = forwardP(inputF,thetaMatrixHiddenLayer)
 H0 = forwardP(a2,thetaMatrixOr)
 print(H0)
